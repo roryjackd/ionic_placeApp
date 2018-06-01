304139c174536d3b3711af57f0f042fb857678f7
roryjackd <roryjackd@yahoo.com>
2018-06-01 12:10:19 -0600

    New place template created

--- src/pages/add-place/add-place.ts
+++ src/pages/add-place/add-place.ts
@@ -1,25 +1,13 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NgForm } from '@angular/forms';
 
-/**
- * Generated class for the AddPlacePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
-@IonicPage()
 @Component({
   selector: 'page-add-place',
   templateUrl: 'add-place.html',
 })
 export class AddPlacePage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  onSubmit(form: NgForm) {
+    console.log(form.value);
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddPlacePage');
-  }
-
 }
